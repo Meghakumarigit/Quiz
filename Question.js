929c5e89587e812810dd1f54a4b157b3f8fa2d43
Megha <107786510+Meghakumarigit@users.noreply.github.com>
2023-07-01 18:30:04 +0530

    some changes in app js

---
+++ Question.js
@@ -0,0 +1,30 @@
+
+
+//Array of the objects
+const quiz = [
+ 	{
+		q:'The size of atoms is nearly ?',
+		option:['0.01 nm','0.1 nm','1 nm','10 nm'],
+		answer:1
+ 	},
+	{
+		q:'A continous image is digitised at ?',
+		option:['Random','Vartex','Contour','Sampling'],
+		answer:3
+	},
+	{
+		q:'How many 9s are there in between 1 to 100 ?',
+		option:['12','25','20','10'],
+		answer:2
+	},
+	{
+		q:'In wireless network an extended service set is a set of ?',
+		option:['Connected basic service sets','All stations','All access points','Connected access points'],
+		answer:0
+	},
+	{
+		q:'Bit error rate provides the information about the type of error ?',
+		option:['True','False','Both'],
+		answer:1
+	}
+]
